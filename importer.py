4923dd8c255e923b1b6efea79d7980ed0c5f424c
Trung-Hieu Ho <piouscity@outlook.com>
2020-02-15 17:15:20 +0700

    explicitly convert value in import_dataset

--- importer.py
+++ importer.py
@@ -7,12 +7,19 @@ def import_dataset(file_path):
 
 
 def import_csv_dataset(file_path):
+    dataset = []
     try:
         with open(file_path) as csv_file:
-            dataset = [
-                {key: float(value) for key, value in row.items()} 
-                for row in csv.DictReader(csv_file, skipinitialspace=True)
-                ]
+            for row in csv.DictReader(csv_file, skipinitialspace=True):
+                converted_row = {}
+                for key, value in row.items():
+                    try:
+                        float_value = float(value)
+                        value = float_value
+                    except ValueError:
+                        pass
+                    converted_row[key] = value
+                dataset.append(converted_row)
     except IOError:
         raise OpenFileException(file_path)
     return dataset
