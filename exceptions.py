78fc1c39a64592859f501e8d9291d0d312e326b4
Trung-Hieu Ho <piouscity@outlook.com>
2020-02-15 16:57:14 +0700

    add open file exception

--- exceptions.py
+++ exceptions.py
@@ -2,3 +2,10 @@
 class BaseException:
     code = 1
     detail = "Base exception"
+
+
+class OpenFileException:
+    code = 20
+    detail = "Can not open file: {}"
+    def __init__(self, file_path):
+        self.detail = self.detail.format(file_path)
--- importer.py
+++ importer.py
@@ -1,12 +1,18 @@
 import os, csv
+from exceptions import OpenFileException
 
 
 def import_dataset(file_path):
-    # file_name, file_ext = os.path.splitext(file_path)
-    with open(file_path) as csv_file:
-        dataset = [{key: float(value) for key, value in row.items()}
-                   for row in csv.DictReader(csv_file, skipinitialspace=True)]
+    return import_csv_dataset(file_path)
 
+
+def import_csv_dataset(file_path):
+    try:
+        with open(file_path) as csv_file:
+            dataset = [{key: float(value) for key, value in row.items()}
+                       for row in csv.DictReader(csv_file, skipinitialspace=True)]
+    except IOError:
+        raise OpenFileException(file_path)
     return dataset
 
 
