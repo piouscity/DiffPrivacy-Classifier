32e2a590c77f8512f8cd316874f10da61f01e81b
Trung-Hieu Ho <piouscity@outlook.com>
2020-02-15 17:26:00 +0700

    add constants

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 
-DATASET_PATH = "winequality-white.csv"
+DATASET_PATH = "datasets/adult.csv"
 TAXO_TREE_PATH = ""
 
 RECORD_PATH = "result/dp_dataset.json"
+
+MISSING_VALUE = "?"
 
 EDP = 2
 STEPS = 54
