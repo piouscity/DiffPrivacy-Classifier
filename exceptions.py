356519395c0ef6d0fc9753ce655396be47f73dfa
Trung-Hieu Ho <piouscity@outlook.com>
2020-02-15 23:47:39 +0700

    Add unsupported file type exception

--- exceptions.py
+++ exceptions.py
@@ -1,11 +1,21 @@
 
-class BaseException:
+class BaseException(Exception):
     code = 1
     detail = "Base exception"
 
 
-class OpenFileException:
-    code = 20
-    detail = "Can not open file: {}"
+class FileException(BaseException):
+    code = 200
+    detail = "Error handling file: {}"
     def __init__(self, file_path):
         self.detail = self.detail.format(file_path)
+
+
+class OpenFileException(FileException):
+    code = 201
+    detail = "Can not open file: {}"
+
+
+class UnsupportedFileTypeException(FileException):
+    code = 202
+    detail = "File type is not supported: {}"
--- importer.py
+++ importer.py
@@ -1,9 +1,13 @@
 import os, csv
-from exceptions import OpenFileException
+from exceptions import OpenFileException, UnsupportedFileTypeException
 
 
 def import_dataset(file_path):
-    return import_csv_dataset(file_path)
+    file_name, file_ext = os.path.splitext(file_path)
+    if file_ext == ".csv":
+        return import_csv_dataset(file_path)
+    else:
+        raise UnsupportedFileTypeException(file_path)
 
 
 def import_csv_dataset(file_path):
