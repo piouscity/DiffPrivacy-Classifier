3a5cff814d6ff3a5f52ee9b63f0aff313f48261f
Trung-Hieu Ho <piouscity@outlook.com>
2020-02-15 16:59:00 +0700

    alter minor format

--- importer.py
+++ importer.py
@@ -9,8 +9,10 @@ def import_dataset(file_path):
 def import_csv_dataset(file_path):
     try:
         with open(file_path) as csv_file:
-            dataset = [{key: float(value) for key, value in row.items()}
-                       for row in csv.DictReader(csv_file, skipinitialspace=True)]
+            dataset = [
+                {key: float(value) for key, value in row.items()} 
+                for row in csv.DictReader(csv_file, skipinitialspace=True)
+                ]
     except IOError:
         raise OpenFileException(file_path)
     return dataset
