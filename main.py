33fee67aff034ede7d65e7a76bc7b4df7c4385e8
Trung-Hieu Ho <piouscity@outlook.com>
2020-02-17 23:21:20 +0700

    create validator

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import traceback
 
 from exceptions import BaseException
+from validator import check_valid_taxonomy_tree
 from importer import import_dataset, import_taxonomy_tree
 from exporter import export_dataset
 from settings import DATASET_PATH, TAXO_TREE_PATH, RECORD_PATH, EDP, STEPS
@@ -9,6 +10,7 @@ from protecter import generate_dp_dataset
 try:
     dataset = import_dataset(DATASET_PATH)
     taxo_tree = import_taxonomy_tree(TAXO_TREE_PATH)
+    check_valid_taxonomy_tree(taxo_tree, dataset)
     private_dataset, attribute_mapping = generate_dp_dataset(
         dataset, taxo_tree, EDP, STEPS
         )
---
+++ validator.py
@@ -0,0 +1,3 @@
+
+def check_valid_taxonomy_tree(taxo_tree, dataset):
+    pass
