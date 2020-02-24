2952c74a0fb071a7ec942fa6841279517ab841d2
duc.nk <1612845@student.hcmus.edu.vn>
2020-02-25 02:15:04 +0700

    export csv file

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ IGNORE_CHECK = False    # Only ignore if taxonomy tree is ensured to be valid
 # File paths
 DATASET_PATH = "datasets/adult-1000.csv"
 TAXO_TREE_PATH = "taxonomy-trees/adult.json"
-RECORD_PATH = ""
+RECORD_PATH = "private_datasets/adult-1000.csv"
 
 # Dataset setting
 CLASS_ATTRIBUTE = "class"
