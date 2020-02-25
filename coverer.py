b6a9c2a98bb96f804fab838600b7e1ce95b80d2c
Trung-Hieu Ho <piouscity@outlook.com>
2020-02-25 14:07:55 +0700

    declare taxonomy value mapper

--- coverer.py
+++ coverer.py
@@ -1,4 +1,8 @@
 
-def generate_dp_dataset(dataset, taxo_tree, edp, steps):
-    pass
+class TaxonomyMapper:
+    def __init__(self, taxo_tree):
+        pass
 
+
+def generate_dp_dataset(dataset, taxo_tree, edp, steps):
+    taxo_mapper = TaxonomyMapper(taxo_tree)
