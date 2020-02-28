4e3698999830ba540cfe6703f67c53edc3806cb7
Trung-Hieu Ho <piouscity@outlook.com>
2020-02-28 22:42:07 +0700

    implement TaxonomValueMapper (constructor)

--- coverer.py
+++ coverer.py
@@ -1,8 +1,37 @@
+from validator import count_float_attribute
+from settings import TAXO_ROOT, TAXO_NODE_NAME, TAXO_NODE_CHILD, TAXO_FROM, \
+   TAXO_TO
 
-class TaxonomyMapper:
+
+class TaxonomyValueMapper:
+    leaf_list = {}
+    parent_list = {}
     def __init__(self, taxo_tree):
-        pass
+        for att in taxo_tree:
+            if TAXO_ROOT in taxo_tree[att]: # Category attribute
+                root = taxo_tree[att][TAXO_ROOT]
+                root_value = root[TAXO_NODE_NAME]
+                self.parent_list[att] = {}
+                leafs = self.__scan_tree(att, root)   
+                self.leaf_list[att] = {root_value: leafs}
+
+    def __scan_tree(self, att, node):
+        node_value = node[TAXO_NODE_NAME]
+        child_list = node[TAXO_NODE_CHILD]
+        if not child_list:   # Is a leaf
+            self.parent_list[att][node_value] = []
+            return [node_value]
+        leafs = []
+        for child in child_list:
+            new_leafs = self.__scan_tree(att, child)
+            leafs.extend(new_leafs)
+        for leaf in leafs:
+            self.parent_list[att][leaf].append(node_value)
+        return leafs
 
 
 def generate_dp_dataset(dataset, taxo_tree, edp, steps):
-    taxo_mapper = TaxonomyMapper(taxo_tree)
+    float_att_cnt = count_float_attribute(dataset)
+    single_edp = edp / 2 / (float_att_cnt + 2*steps)
+    taxo_mapper = TaxonomyValueMapper(taxo_tree)
+
