8597d31e57282362e50bd25bd38d1200fe71d57e
duc.nk <1612845@student.hcmus.edu.vn>
2020-05-23 08:45:10 +0700

    add import/export matrix

--- src/file_handler.py
+++ src/file_handler.py
@@ -76,9 +76,30 @@ def import_json_taxonomy_tree(file_path):
     return taxo_tree
 
 
+def import_csv_matrix(file_path):
+    reader = csv.reader(open(file_path), delimiter=",")
+    data = list(reader)
+    return [[float(item) for item in row] for row in data]
+
+
 def import_matrix(file_path):
-    return []
+    file_name, file_ext = os.path.splitext(file_path)
+    if file_ext == CSV_EXT:
+        return import_csv_matrix(file_path)
+    raise UnsupportedFileTypeException(file_path)
+
+
+def export_csv_matrix(file_path, matrix):
+    try:
+        with open(file_path, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerows(matrix)
+    except IOError:
+        raise OpenFileException(file_path)
 
 
 def export_matrix(file_path, matrix):
-    pass
+    file_name, file_ext = os.path.splitext(file_path)
+    if file_ext == CSV_EXT:
+        return export_csv_matrix(file_path, matrix)
+    raise UnsupportedFileTypeException(file_path)
