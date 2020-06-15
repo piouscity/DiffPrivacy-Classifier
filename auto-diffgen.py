a9d96b75f3750e104ff792d567bff02706e2206b
Trung-Hieu Ho <piouscity@outlook.com>
2020-06-15 16:03:25 +0700

    alter main file

--- auto-diffgen.py
+++ auto-diffgen.py
@@ -1,10 +1,14 @@
 import logging
+import math
 
-from settings import DATASET_PATH, TAXO_TREE_PATH, EDP
+from settings import DATASET_PATH, TAXO_TREE_PATH, EDP, LOG_FILE, LOG_LEVEL
+from src.file_handler import import_dataset, import_taxonomy_tree
 from src.coverer.DatasetNode import DatasetNode
+from src.coverer.CutCandidateSet import CutCandidateSet
 
 
 MAX_STEP = 16
+SQ2 = math.sqrt(2)
 
 logging.basicConfig(filename=LOG_FILE, filemode='w',level=LOG_LEVEL)
 dataset = import_dataset(DATASET_PATH)
@@ -19,5 +23,7 @@ for i in range(MAX_STEP):
     if index < 0:
         break
     cut_set.specialize_candidate(index)
+    impact = data_root.predict_noise_impact(EDP/2*SQ2, cut_set.class_list)
+    print("Level {0}, noise impact is {1}".format(i+1, impact))
     cut_set.determine_new_splits(None)
     cut_set.calculate_candidate_score()
